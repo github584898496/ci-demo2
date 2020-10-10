6e116aa6c262b44791a9c0584cf4033a5dba3ef8
zsq <wy584898496@163.com>
2020-10-10 18:06:05 +0800

    新增Query

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -18,4 +18,10 @@ public class UserController {
        {
            return "user--delete!";
        }
+	   
+	   @GetMapping("query")
+       public String query()
+       {
+           return "user--query!";
+       }
 }
